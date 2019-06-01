ee7c236c9979e7a5e0486bf15a06006b8752d0ba
David Gallegos <david.gallegos@outlook.com>
2019-06-01 14:15:49 -0700

    connection established with DB

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Green Chile Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Red Chile Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bacon Chile Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Mushroom Chile Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Meatless Chile Burger", false);
